274fda00b5cee6f9bfd4b4d2b784e3844f68dbba
Maxim Sadofyev <cfljamtd@gmail.com>
2021-02-16 22:10:13 +0500

    GitHub Pages

--- src/app/components/expansion-panel-content/expansion-panel-content.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-    selector: 'app-expansion-panel-content',
-    templateUrl: './expansion-panel-content.component.html',
-    styleUrls: ['./expansion-panel-content.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class ExpansionPanelContentComponent {}
--- src/app/components/expansion-panel-header/expansion-panel-header.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-    selector: 'app-expansion-panel-header',
-    templateUrl: './expansion-panel-header.component.html',
-    styleUrls: ['./expansion-panel-header.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class ExpansionPanelHeaderComponent {}
